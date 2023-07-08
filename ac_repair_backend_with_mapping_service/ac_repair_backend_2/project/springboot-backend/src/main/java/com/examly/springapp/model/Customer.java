d49acd664577c1734d0bc280f832b9d65d84996d
shoel10 <97252612+shoel10@users.noreply.github.com>
2023-07-08 12:02:55 +0530

    Update Customer.java

--- ac_repair_backend_with_mapping_service/ac_repair_backend_2/project/springboot-backend/src/main/java/com/examly/springapp/model/Customer.java
+++ ac_repair_backend_with_mapping_service/ac_repair_backend_2/project/springboot-backend/src/main/java/com/examly/springapp/model/Customer.java
@@ -1,7 +1,7 @@
 package com.examly.springapp.model;
 
 
-import jakarta.persistence.*;
+import javax.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,7 +16,7 @@ import lombok.Setter;
 public class Customer {
 
     @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
+     @Column(name = "id")
     private long id;
 
     @Column(name = "name")
