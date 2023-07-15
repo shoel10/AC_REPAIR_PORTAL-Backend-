b123203745716af1743b125cfd581f17e3e543ec
shoel10 <97252612+shoel10@users.noreply.github.com>
2023-07-15 11:16:58 +0530

    Update ResourceNotFoundException.java

--- ac_repair_backend_with_mapping_service/ac_repair_backend_2/project/springboot-backend/src/main/java/com/examly/springapp/exception/ResourceNotFoundException.java
+++ ac_repair_backend_with_mapping_service/ac_repair_backend_2/project/springboot-backend/src/main/java/com/examly/springapp/exception/ResourceNotFoundException.java
@@ -5,7 +5,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value= HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
+    
+    public ResourceNotFoundException(String message) 
+    {
         super(message);
     }
 }
